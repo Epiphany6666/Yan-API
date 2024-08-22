8c099f075eca15fa95d6c87ec16a1ed2502abd97
只会CV <528375822@qq.com>
2024-08-22 16:28:41 +0800

    修改浏览接口文档页

--- yanapi-frontend/src/pages/InterfaceInfo/index.tsx
+++ yanapi-frontend/src/pages/InterfaceInfo/index.tsx
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { PageContainer } from '@ant-design/pro-components';
-import { List, message } from 'antd';
-import { listInterfaceInfoByPageUsingPost } from '@/services/Yan-API-backend/interfaceInfoController';
+import { Card, List, message } from 'antd';
+import {
+  getInterfaceInfoVoByIdUsingGet,
+  listInterfaceInfoByPageUsingPost,
+} from '@/services/Yan-API-backend/interfaceInfoController';
+import { useMatch, useParams } from 'react-router';
 
 const Index: React.FC = () => {
   const [loading, setLoading] = useState(false);
-  const [list, setList] = useState<API.InterfaceInfo[]>([]);
-  const [total, setTotal] = useState<number>(0);
+  const [data, setData] = useState<API.InterfaceInfo>();
+  const params = useParams()
 
-  const loadData = async (current = 1, pageSize = 5) => {
+  const loadData = async () => {
+    if (!params.id) {
+      message.error('参数不存在')
+    }
     setLoading(true)
     try {
-      const res = await listInterfaceInfoByPageUsingPost({
-        current, pageSize
+      const res = await getInterfaceInfoVoByIdUsingGet({
+        id: Number(params.id)
       })
-      setList(res?.data?.records ?? [])
-      setTotal(res?.data?.total ?? [])
+      setData(res.data)
     } catch (error) {
       message.error('请求失败，' + error.message);
     }
@@ -26,35 +32,10 @@ const Index: React.FC = () => {
   }, [])
 
   return (
-    <PageContainer title='在线接口开放平台'>
-      <List
-        className='my-list'
-        loading={loading}
-        itemLayout='horizontal'
-        dataSource={list}
-        renderItem={(item) => (
-          <List.Item
-            actions={[<a key='list-loadmore-edit'>查看</a>]}
-          >
-            <List.Item.Meta
-              title={<a href='https://ant.design'>{item.name}</a>}
-              description={item.description}
-            />
-          </List.Item>
-        )}
-        pagination={
-          {
-            pageSize: 2,
-            total: total,
-            showTotal(total:number) {
-              return '总数' + total
-            },
-            onChange: (page, pageSize) => {
-              loadData(page, pageSize)
-            },
-          }
-        }
-      />
+    <PageContainer title='查看接口'>
+      <Card>
+
+      </Card>
     </PageContainer>
   );
 };
