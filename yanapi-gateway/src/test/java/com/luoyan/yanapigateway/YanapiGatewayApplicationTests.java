490b956bd47e5c8da31e605293a5dee579a85173
只会CV <528375822@qq.com>
2024-08-23 20:09:54 +0800

    搭建网关模块

---
+++ yanapi-gateway/src/test/java/com/luoyan/yanapigateway/YanapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoyan.yanapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanapiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
