3f94dd471f157cf3175947fe3f88ef45c2e910b0
只会CV <528375822@qq.com>
2024-08-24 08:44:59 +0800

    实现黑白名单

--- yanapi-gateway/src/main/java/com/luoyan/yanapigateway/CustomGlobalFilter.java
+++ yanapi-gateway/src/main/java/com/luoyan/yanapigateway/CustomGlobalFilter.java
@@ -4,15 +4,48 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 @Slf4j
 public class CustomGlobalFilter implements GlobalFilter, Ordered {
 
+    private static final List<String> IP_WHITE_LIST = Arrays.asList("127.0.0.1");
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1. 请求日志
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("请求唯一标识：" + request.getId());
+        log.info("请求路径：" + request.getPath().value());
+        log.info("请求方法：" + request.getMethod());
+        log.info("请求参数：" + request.getQueryParams());
+        String sourceAddress = request.getLocalAddress().getHostString();
+        log.info("请求来源地址：" + sourceAddress);
+        log.info("请求来源地址：" + request.getRemoteAddress());
+
+        // 拿到响应对象
+        ServerHttpResponse response = exchange.getResponse();
+        // 2. 访问控制，黑白名单
+        if (!IP_WHITE_LIST.contains(sourceAddress)) {
+            response.setStatusCode(HttpStatus.FORBIDDEN);
+            return response.setComplete();
+        }
+
+        // 4. 用户鉴权（判断 ak, sk 是否合法）
+        // 5. 请求的 Mock 接口是否存在？
+        // 6. 请求参数，调用 Mock 接口校验
+        // 7. 响应日志
+        // 8. 调用次数，接口调用最大次数 + 1
+        // 9. 调用限流，返回一个规范的错误码
         log.info("custom global filter");
         return chain.filter(exchange);
     }
