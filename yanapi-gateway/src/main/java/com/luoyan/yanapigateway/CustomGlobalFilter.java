4fdad60b90a73278eaffcb9dad73636062266c88
只会CV <528375822@qq.com>
2024-08-25 15:10:41 +0800

    网关统一用户鉴权

--- yanapi-gateway/src/main/java/com/luoyan/yanapigateway/CustomGlobalFilter.java
+++ yanapi-gateway/src/main/java/com/luoyan/yanapigateway/CustomGlobalFilter.java
@@ -1,9 +1,11 @@
 package com.luoyan.yanapigateway;
 
+import com.luoyan.yanapiclientsdk.utils.SignUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -41,6 +43,44 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
         }
 
         // 4. 用户鉴权（判断 ak, sk 是否合法）
+        HttpHeaders headers = request.getHeaders();
+        // 从请求头中获取参数
+        String accessKey = headers.getFirst("accessKey");
+        String nonce = headers.getFirst("nonce");
+        String timestamp = headers.getFirst("timestamp");
+        String sign = headers.getFirst("sign");
+        String body = headers.getFirst("body");
+
+        // todo 实际情况应该是去数据库中查是否已分配给用户
+        if (!accessKey.equals("luoyan")) {
+            throw new RuntimeException("无权限");
+        }
+
+        // 直接校验如果随机数大于1万，则抛出异常，并提示“无权限”
+        if (Long.parseLong(nonce) > 10000) {
+            throw new RuntimeException("无权限");
+        }
+
+        // 首先,获取当前时间的时间戳,以秒为单位
+        // System.currentTimeMillis()返回当前时间的毫秒数，除以1000后得到当前时间的秒数。
+        Long currentTime = System.currentTimeMillis() / 1000;
+        // 定义一个常量FIVE_MINUTES,表示五分钟的时间间隔(乘以60,将分钟转换为秒,得到五分钟的时间间隔)。
+        final Long FIVE_MINUTES = 60 * 5L;
+        // 判断当前时间与传入的时间戳是否相差五分钟或以上
+        // Long.parseLong(timestamp)将传入的时间戳转换成长整型
+        // 然后计算当前时间与传入时间戳之间的差值(以秒为单位),如果差值大于等于五分钟,则返回true,否则返回false
+        if ((currentTime - Long.parseLong(timestamp)) >= FIVE_MINUTES) {
+            // 如果时间戳与当前时间相差五分钟或以上，调用handleNoAuth(response)方法进行处理
+            return handleNoAuth(response);
+        }
+
+        // todo 实际情况是从数据库中查出 secretKey
+        String serverSign = SignUtils.genSign(body, "abcdefgh");
+        // 如果生成的签名不一致，则抛出异常，并提示“无权限”
+        if (!sign.equals(serverSign)) {
+            throw new RuntimeException("无权限");
+        }
+
         // 5. 请求的 Mock 接口是否存在？
         // 6. 请求参数，调用 Mock 接口校验
         // 7. 响应日志
@@ -53,5 +93,10 @@ public class CustomGlobalFilter implements GlobalFilter, Ordered {
     @Override
     public int getOrder() {
         return -1;
+    }
+
+    public Mono<Void> handleNoAuth(ServerHttpResponse response) {
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+        return response.setComplete();
     }
 }
